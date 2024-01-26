2d21a6985f639c8098e5941ba7747a33a4d6dc7e
Peter Palacios <pj1palacios@gmail.com>
2024-01-26 07:33:52 +0100

    removed unused comments

--- Labs/Mod3.NewtonRaphson/src/MinMax.java
+++ Labs/Mod3.NewtonRaphson/src/MinMax.java
@@ -9,15 +9,6 @@ public class MinMax
         y = Math.random()*5;
     }
 
-//   Apply the Newton-Raphson's method  to develop a function double findMinMax(…) that returns a solution that is either a minimum value or a maximum value for z. The program finds a local minimum or a local maximum when the difference between the new solution and the previous one is smaller than 0.00001 within 10000 iterations. Otherwise, it shows Infinity as output.
-//
-//    Let function z = x4+2y6 – xy – x + 2. Show the post-conditions for z.
-//    The preconditions for x and y are shown as below. Feel free to choose an arbitrary initial guess values x0 and y0 that meet the following precondition.
-//
-
-
-    //previous one is smaller than 0.00001 within 10000 iterations.
-//|x| ≤ 6 and |y| ≤ 5
     public void findMinMax()
     {
         if( x>6 || y >5)
@@ -39,7 +30,6 @@ public class MinMax
                 break;
             }
 
-
             if(Math.abs(nextX-x) < 0.00001)
             {
                 double MinMax = ValueOfFunction(x,y);
@@ -53,11 +43,6 @@ public class MinMax
 
         System.out.println("Infinity");
 
-
-
-        //xn+1 = xn – f '(xn) / f ''(xn)
-
-        //next = x - ((getPrimeOfX(x,y)/getDoublePrimeOfX(x))
 
 
     }
