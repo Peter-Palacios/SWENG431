095eff851e85e060b77144f0453b0a6f7c8c91d3
Peter Palacios <pj1palacios@gmail.com>
2024-02-04 00:21:53 +0100

    corrected CheckTriangleSides logic

--- Labs/Mod4.RobustnessTesting/src/Main.java
+++ Labs/Mod4.RobustnessTesting/src/Main.java
@@ -44,15 +44,15 @@ public class Main
 
     private static boolean CheckIfSidesAreTriangle(int a, int b, int c)
     {
-        if(a > b+c)
+        if(a >= b+c)
         {
             return false;
         }
-        if(b>a+c)
+        if(b >= a+c)
         {
             return false;
         }
-        if(c>a+b)
+        if(c >= a+b)
         {
             return false;
         }
